3340eea30a667f52b639ab72ff0917ad12c167cd
root <root@vps77234.inmotionhosting.com>
2023-02-28 00:13:35 -0500

    Move ealiases lookup into custom script.  This was undocumented behavior that broke in an exim update

--- test_files/kruskal_test.py
+++ test_files/kruskal_test.py
@@ -17,7 +17,7 @@ def test_main_forwards(received, expected, parse_matcher):
       forwarded = expected,
       accepted = set(),
       bounced = set())
-  assert exim.verify_addresses({received}) == {received}
+  assert exim.verify_addresses({received}, conf_suffix="test") == {received}
 
 @pytest.mark.parametrize("received,expected", [
     ("sueandmichael@kruskal.net", {"michael879@gmail.com", "sueacano@gmail.com"}),
@@ -29,7 +29,7 @@ def test_groups(received, expected, parse_matcher):
       forwarded = expected,
       accepted = set(),
       bounced = set())
-  assert exim.verify_addresses({received}) == {received}
+  assert exim.verify_addresses({received}, conf_suffix="test") == {received}
 
 @pytest.mark.parametrize("received,expected", [
     ("amazon.com@kruskal.net", "kruskal@gmail.com"),
@@ -42,7 +42,7 @@ def test_regex(received, expected, parse_matcher):
       forwarded = {expected},
       accepted = set(),
       bounced = set())
-  assert exim.verify_addresses({received}) == {received}
+  assert exim.verify_addresses({received}, conf_suffix="test") == {received}
 
 @pytest.mark.parametrize("received,expected", [
     ("bloomingdales.com@kruskal.net", "jbkessler@gmail.com"),
@@ -54,7 +54,7 @@ def test_regex_exceptions(received, expected, parse_matcher):
       forwarded = expected,
       accepted = set(),
       bounced = set())
-  assert exim.verify_addresses({received}) == {received}
+  assert exim.verify_addresses({received}, conf_suffix="test") == {received}
 
 @pytest.mark.parametrize("received,expected", [
     ("sueandmichael@kruskal.net", {"michael879@gmail.com", "sueacano@gmail.com"}),
@@ -66,7 +66,7 @@ def test_groups(received, expected, parse_matcher):
       forwarded = expected,
       accepted = set(),
       bounced = set())
-  assert exim.verify_addresses({received}) == {received}
+  assert exim.verify_addresses({received}, conf_suffix="test") == {received}
 
 @pytest.mark.parametrize("tld", ["com", "edu", "gov", "info", "net", "org", "us"])
 def test_tld_valid(tld, parse_matcher):
@@ -76,7 +76,7 @@ def test_tld_valid(tld, parse_matcher):
       forwarded = "kruskal@gmail.com",
       accepted = set(),
       bounced = set())
-  assert exim.verify_addresses({email}) == {email}
+  assert exim.verify_addresses({email}, conf_suffix="test") == {email}
 
 def test_tld_invalid(parse_matcher):
   invalid = "something.ooo@kruskal.net"
@@ -85,7 +85,7 @@ def test_tld_invalid(parse_matcher):
       forwarded = set(),
       accepted = set(),
       bounced = invalid)
-  assert exim.verify_addresses({invalid}) == set()
+  assert exim.verify_addresses({invalid}, conf_suffix="test") == set()
 
 @pytest.mark.parametrize("expired", [
     "expired@kruskal.net",
@@ -98,4 +98,4 @@ def test_tld_invalid(expired, parse_matcher):
       accepted = set(),
       bounced = set(),
       expired = expired)
-  assert exim.verify_addresses({expired}) == set()
+  assert exim.verify_addresses({expired}, conf_suffix="test") == set()
